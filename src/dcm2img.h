ef8c915558fd1036807a32c783722accc62768ce
DraconPern <draconpern@hotmail.com>
2017-07-16 04:19:13 -0500

    remove dcm2img

--- src/dcm2img.h
+++
@@ -1,6 +0,0 @@
-#include <boost/filesystem.hpp>
-#include <wx/image.h>
-
-void dcm2img(boost::filesystem::path filename, int width, int height, wxImage &image);
-void RegisterCodecs();
-void DeregisterCodecs();
